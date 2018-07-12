0d81cbf5050e4aaf647fdb3b3c9b42c5b17cb61a
UdayakumarU <31164995+UdayakumarU@users.noreply.github.com>
2018-07-12 13:09:02 +0530

    Create vowel_consonant.py

---
+++ vowel_consonant.py
@@ -0,0 +1,11 @@
+#vowel or consonant
+n = input()
+n = n.lower()
+vowel =['a','e','i','o','u']
+con =['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if n in vowel:
+	print("Vowel")
+elif n in con:
+	print("Consonant")
+else :
+	print("Invalid")
